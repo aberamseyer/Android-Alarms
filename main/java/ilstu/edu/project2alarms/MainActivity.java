3a2f6b468adc86d0bf2de97c810cf04c21b935b8
Ramseyer, Abe <aramsey@ilstu.edu>
2016-11-05 12:28:51 -0500

    made a comment
    changed app title
    
    Animated the fab and gave it 2 icons, changed the theme of the app
    
    made the large fab animation turn farther
    
    the floating fabs now disappear when one is tapped
    
    Created fab animation and buttons to go with it

--- main/java/ilstu/edu/project2alarms/MainActivity.java
+++ main/java/ilstu/edu/project2alarms/MainActivity.java
@@ -1,15 +1,22 @@
 package ilstu.edu.project2alarms;
+// Abe wrote this
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Boolean isFabOpen = false;
+    private FloatingActionButton fab,fab1,fab2;
+    private Animation fab1_open,fab2_open,fab_close,rotate_forward,rotate_backward;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +25,60 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab = (FloatingActionButton)findViewById(R.id.fab);
+        fab1 = (FloatingActionButton)findViewById(R.id.fab1);
+        fab2 = (FloatingActionButton)findViewById(R.id.fab2);
+        fab1_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab1_open);
+        fab2_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab2_open);
+
+        fab_close = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close);
+        rotate_backward = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_backward);
+        rotate_forward = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_forward);
+
+        fab.setOnClickListener(this);
+        fab1.setOnClickListener(this);
+        fab2.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        int tappedButton = v.getId();
+        switch (tappedButton) {
+            case R.id.fab:
+                animateFab();
+                Log.d("aramsey", "Big fAB tapped");
+                break;
+            case R.id.fab1:
+                Log.d("aramsey", "fab 1 tapped");
+                animateFab();
+                break;
+            case R.id.fab2:
+                Log.d("aramsey", "fab 2 tapped");
+                animateFab();
+                break;
+        }
+    }
+
+    public void animateFab() {
+        if(isFabOpen) {
+            fab.startAnimation(rotate_backward);
+            fab2.startAnimation(fab_close);
+            fab1.startAnimation(fab_close);
+            fab1.setClickable(false);
+            fab2.setClickable(false);
+            isFabOpen = false;
+        }
+        else {
+            fab.startAnimation(rotate_forward);
+            fab1.startAnimation(fab1_open);
+            fab2.startAnimation(fab2_open);
+            fab1.setClickable(true);
+            fab2.setClickable(true);
+            isFabOpen = true;
+            System.out.println("opened the big one");
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
